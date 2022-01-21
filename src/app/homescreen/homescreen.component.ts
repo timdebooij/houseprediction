a3214fae6774f66055ef1711c502c2ad08d395fa
tim <timdebooij9@hotmail.com>
2022-01-21 16:19:38 +0100

    api wake upp call

--- src/app/homescreen/homescreen.component.ts
+++ src/app/homescreen/homescreen.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import {Router, RouterModule} from '@angular/router'
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 
@@ -10,7 +12,26 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     templateUrl: './homescreen.component.html',
     styleUrls: ['./homescreen.component.css']
   })
+  @Injectable()
   export class HomescreenComponent {
+    config: Config = new Config();
+    path = "";
+
+    constructor(private http: HttpClient){
+      this.showConfig()
+    }
+
+    getConfig() {
+      // now returns an Observable of Config
+      this.path = "https://api-hcaid.herokuapp.com/predictions/1/1/1/1/1/1/1/1/";
+      return this.http.get<Config>(this.path);
+    }
+
+    showConfig() {
+      this.getConfig()
+        // clone the data object, using its known Config shape
+        .subscribe((data: Config) => this.config = {...data});
+    }
 
     toPredictionPage(){
       console.log("to prediction page")
@@ -23,4 +44,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     predict(){
 
     }
+  }
+
+  export class Config {
+    accuracy: number = 0;
+    prediction: number = 0;
   }
